210b3ab77a00a94accdd0a37dc1f2cdaaceff803
Eyole9 <117469913+Eyole9@users.noreply.github.com>
2022-11-18 21:58:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 // write your code here
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() 
+{
+  int num, value;
+  cout << " Enter Number of values desired" << endl;
+  num =1;
+  cin >> value;
+  while (num <= value )
+    {
+    cout << rand()% 100;
+      cout << " ";
+     num = num + 1;
+  }
+  return 0;
+    
+  }
